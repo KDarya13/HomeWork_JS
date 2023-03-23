79e375ebad9fcf63ef7f7a817d39ffd8f2681cfd
Darya <Darsy_13@bk.ru>
2023-03-23 11:02:59 +0300

    add task2 sem2

---
+++ home_work_task2_sem2.js
@@ -0,0 +1,11 @@
+//Задание 2
+//Перепишите код к тернарному оператору
+
+let test = true;
+/*if (test === true) {
+console.log('+++');
+} else {
+console.log('---');
+}*/
+
+console.log((test === true) ? '+++' : '---');
